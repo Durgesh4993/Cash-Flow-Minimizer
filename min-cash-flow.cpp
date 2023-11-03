aa290c188abc57e44f58438bcadf41df79ccb3ee
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-11-03 18:58:50 +0530

    Update min-cash-flow.cpp

--- min-cash-flow.cpp
+++ min-cash-flow.cpp
@@ -11,6 +11,7 @@ class bank{
 int getMinIndex(bank listOfNetAmounts[],int numBanks){  
     int min=INT_MAX, minIndex=-1;
     for(int i=0;i<numBanks;i++){
+        
         if(listOfNetAmounts[i].netAmount == 0) continue;
         
         if(listOfNetAmounts[i].netAmount < min){
